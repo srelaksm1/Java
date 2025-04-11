26a80c1fcc33cf0c3e043c81472056bca8be0d82
Sreelakshmi M <156561183+srelaksm1@users.noreply.github.com>
2025-04-11 16:01:20 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,17 @@
+// File: Circle.java
+package graphics;
+import graphics.Figure;
+
+public class Circle implements Figure {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
